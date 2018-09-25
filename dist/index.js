409ffa11755e8625d49de15827b0dec6f587b611
Mahmoud Maamoun <mmaamoun@apprisemobile.com>
2018-09-25 14:57:42 -0400

    changes

--- dist/index.js
+++ dist/index.js
@@ -13,6 +13,8 @@ var _propTypes = require("prop-types");
 
 var _propTypes2 = _interopRequireDefault(_propTypes);
 
+require("@babel/polyfill");
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
